ab693897ed20c672c0f19ba932b03debce0bea0c
Lokesh Kumar <lk040904@gmail.com>
2025-11-21 02:11:25 +0530

    Commit 3

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.get('/', (req, res) => {
         <div class="container">
           <h1>🚀 Hello World!</h1>
           <p>Welcome to My CI/CD Pipeline Demo</p>
-            <p>This is Lokesh</p>
+            <p>Sending E-mail......</p>
           <p><strong>Deployed via Jenkins + Docker + AWS</strong></p>
           <div>
             <span class="badge">✓ GitHub Integration</span>
